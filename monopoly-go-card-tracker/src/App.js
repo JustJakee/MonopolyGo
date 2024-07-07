554bb54d7138a4eab1c288897eb93122533c6f7a
Jake Bertish <bertishprofessional@gmail.com>
2024-07-07 15:24:07 -0500

    Clean/Refactor Code to prepare for google/DB integration
    Code needed a huge refresh , instead of hard coding values we have a more dynamic setup now. Data is passed between menu and header so progress bar can work. Now we are ready for database and google login integration for users to save their progress.

--- monopoly-go-card-tracker/src/App.js
+++ monopoly-go-card-tracker/src/App.js
@@ -1,15 +1,18 @@
 import './css/App.css';
+import React, { useState } from 'react';
 import Menu from './components/Menu';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import LogIn from './components/LogIn';
 
 function App() {
+  const [progress, setProgress] = useState(0);
+
   return (
     <div className="App">
       <LogIn />
-      <Header />
-      <Menu />
+      <Header progress={progress} />
+      <Menu setProgress={setProgress} />
       <Footer />
     </div>
   );
